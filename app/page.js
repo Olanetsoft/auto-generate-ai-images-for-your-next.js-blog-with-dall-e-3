869907557bf7e577ba7621ccc6ce306f7aecc934
Olanetsoft <heedris2olubisi@gmail.com>
2024-07-25 01:35:19 +0100

    fix: post title

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-2xl">
         <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
-          Auto-generate AI Images for your Next.js blog with Dall-E 3
+          Create AI-generated Images for Your Next.js Blog With Dall-E 3
         </h1>
         <ImageForm />
       </div>
